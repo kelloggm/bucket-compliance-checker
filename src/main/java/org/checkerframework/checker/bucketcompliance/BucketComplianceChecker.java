dd8461d512df84f49023b7241e4f122a99de03fb
Martin Kellogg <kelloggm@cs.washington.edu>
2020-02-25 17:08:16 -0800

    add subcheckers

--- src/main/java/org/checkerframework/checker/bucketcompliance/BucketComplianceChecker.java
+++ src/main/java/org/checkerframework/checker/bucketcompliance/BucketComplianceChecker.java
@@ -1,8 +1,22 @@
 package org.checkerframework.checker.bucketcompliance;
 
+import org.checkerframework.checker.objectconstruction.ObjectConstructionChecker;
 import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.common.value.ValueChecker;
+
+import java.util.LinkedHashSet;
 
 /**
  * This is the entry point for pluggable type-checking.
  */
-public class BucketComplianceChecker extends BaseTypeChecker {}
+public class BucketComplianceChecker extends BaseTypeChecker {
+
+    @Override
+    protected LinkedHashSet<Class<? extends BaseTypeChecker>> getImmediateSubcheckerClasses() {
+        LinkedHashSet<Class<? extends BaseTypeChecker>> checkers =
+                super.getImmediateSubcheckerClasses();
+        checkers.add(ObjectConstructionChecker.class);
+        checkers.add(ValueChecker.class);
+        return checkers;
+    }
+}
